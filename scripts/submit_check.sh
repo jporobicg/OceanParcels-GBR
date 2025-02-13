0d929fe31d231f7981f82f1a40bceeeb665d3ee9
por07g <Javier.Porobicgarate@csiro.au>
2025-02-13 15:24:38 +1100

    update run for missing files

---
+++ scripts/submit_check.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --account=OD-232995
+#SBATCH --job-name=check_files
+#SBATCH --output=check_files_%j.log
+#SBATCH --error=check_files_%j.err
+#SBATCH --time=02:00:00
+#SBATCH --mem=4GB
+#SBATCH --ntasks=1
+
+bash Failed_runs.sh 
