b94bb28a261a69aed00337c56303dc51fe56b37c
por07g <Javier.Porobicgarate@csiro.au>
2025-03-03 14:06:48 +1100

    update model runs failed

--- jobs/Failed_Coral_run_job_9.sh
+++ jobs/Failed_Coral_run_job_9.sh
@@ -5,10 +5,9 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --output=/dev/null  # Discard output files
 #SBATCH --error=/dev/null   # Discard error files
-#SBATCH --array=930,2129,2929,3260%700  # Only run failed jobs
-
+#SBATCH --array=0-3805%700
 module load python
 
 (( run = SLURM_ARRAY_TASK_ID ))
 
-python ../src/main.py     $run 2015-11-28 T20:00 2015-12-02 T23:59 1000
+python ../src/main.py     $run 2018-10-27 T20:00 2018-10-31 T23:59 1000
